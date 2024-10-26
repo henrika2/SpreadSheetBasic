1b5ed78872fc7b0e994ec26e82b9afb39e974bee
Lee-vin <vincent.li.tiancheng@gmail.com>
2024-10-25 19:13:44 -0600

    Update SpreadsheetGUI.razor.cs

--- GUI.Client/Pages/SpreadsheetGUI.razor.cs
+++ GUI.Client/Pages/SpreadsheetGUI.razor.cs
@@ -14,7 +14,24 @@ using System;
 using System.Diagnostics;
 
 /// <summary>
-///  FIXME.
+/// Author:    Lan Quang Huynh
+/// Partner:   Tiancheng Li
+/// Date:      12-25-2024
+/// Course:    CS 3500, University of Utah, School of Computing
+/// Copyright: CS 3500 and Tiancheng Li - This work may not
+///            be copied for use in Academic Coursework.
+///
+/// I, Tiancheng Li, certify that I wrote this code from scratch and
+/// did not copy it in part or whole from another source.  All
+/// references used in the completion of the assignments are cited.
+/// in my README file.
+///
+/// File Contents
+/// This file contain a graphical user interface for the Spreadsheet application.
+/// </summary>
+
+/// <summary>
+///  SpreadsheetGUI provides a graphical user interface for the Spreadsheet application.
 ///  <remarks>
 ///    <para>
 ///      This is a partial class because class SpreadsheetGUI is also automatically
@@ -38,9 +55,10 @@ using System.Diagnostics;
 /// </summary>
 public partial class SpreadsheetGUI
 {
-    // Define max rows and columns allowed
+    // Define max rows and columns allowed.
     private const int MaxColumns = 26;
 
+    // Define max colunm and columns allowed.
     private const int MaxRows = 99;
 
     // Current grid size (initialized to 10x10)
@@ -52,7 +70,7 @@ public partial class SpreadsheetGUI
     /// <summary>
     ///    Gets the alphabet for ease of creating columns.
     /// </summary>
-    private static char[ ] Alphabet { get; } = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+    private static char[] Alphabet { get; } = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
 
     /// <summary>
     ///   Gets or sets the javascript object for this web page that allows
@@ -60,7 +78,7 @@ public partial class SpreadsheetGUI
     /// </summary>
     private IJSObjectReference? JSModule { get; set; }
 
-   /// <summary>
+    /// <summary>
     ///   Gets or sets FIXME.
     /// </summary>
     private string FileSaveName { get; set; } = "Spreadsheet.sprd";
@@ -75,13 +93,13 @@ public partial class SpreadsheetGUI
     ///   <para> Gets or sets the data for all of the cells in the spreadsheet GUI. </para>
     ///   <remarks>Backing Store for HTML</remarks>
     /// </summary>
-    private string[,] CellsBackingStore { get; set; } = new string[ 100, 26 ];
+    private string[,] CellsBackingStore { get; set; } = new string[100, 26];
 
     /// <summary>
     ///   <para> Gets or sets the html class string for all of the cells in the spreadsheet GUI. </para>
     ///   <remarks>Backing Store for HTML CLASS strings</remarks>
     /// </summary>
-    private string[,] CellsClassBackingStore { get; set; } = new string[ 100, 26 ];
+    private string[,] CellsClassBackingStore { get; set; } = new string[100, 26];
 
     /// <summary>
     ///   Gets or sets a value indicating whether we are showing the save "popup" or not.
@@ -99,15 +117,15 @@ public partial class SpreadsheetGUI
     ///   true if the spreadsheet is changed.
     /// </returns>
     [JSInvokable]
-    public bool HasSpreadSheetChanged(  )
+    public bool HasSpreadSheetChanged()
     {
         return this.currentSpreadSheet!.Changed;
     }
 
     /// <summary>
-         ///   Example of how JavaScript can talk "back" to the C# side.
-         /// </summary>
-         /// <param name="message"> string from javascript side. </param>
+    Â  Â  Â /// Â  Example of how JavaScript can talk "back" to the C# side.
+    Â  Â  Â /// </summary>
+    Â  Â  Â /// <param name="message"> string from javascript side. </param>
     [JSInvokable]
     public void TestBlazorInterop(string message)
     {
@@ -121,7 +139,7 @@ public partial class SpreadsheetGUI
     ///     You probably don't need to do anything here.
     ///   </remarks>
     /// </summary>
-    protected override void OnInitialized( )
+    protected override void OnInitialized()
     {
         this.currentSpreadSheet = new();
         InitializeBackingStores();
@@ -135,25 +153,25 @@ public partial class SpreadsheetGUI
     ///   </remarks>
     /// </summary>
     /// <param name="firstRender"> true the very first time the page is rendered.</param>
-    protected async override void OnAfterRender( bool firstRender )
+    protected async override void OnAfterRender(bool firstRender)
     {
-        base.OnAfterRender( firstRender );
+        base.OnAfterRender(firstRender);
 
-        Debug.WriteLine( $"{"OnAfterRenderStart",-30}: {Navigator.Uri} - first time({firstRender}). Remove Me." );
+        Debug.WriteLine($"{"OnAfterRenderStart",-30}: {Navigator.Uri} - first time({firstRender}). Remove Me.");
 
-        if ( firstRender )
+        if (firstRender)
         {
             /////////////////////////////////////////////////
             //
             // The following three lines setup and test the
             // ability for Blazor to talk to javascript and vice versa.
-            JSModule = await JS.InvokeAsync<IJSObjectReference>( "import", "./Pages/SpreadsheetGUI.razor.js" ); // create/read the javascript
-            await JSModule.InvokeVoidAsync( "SetDotNetInterfaceObject", DotNetObjectReference.Create( this ) ); // tell the javascript about us (dot net)
-            await JSModule.InvokeVoidAsync( "TestJavaScriptInterop", "Hello JavaScript!" ); // test that it is working.  You could remove this.
+            JSModule = await JS.InvokeAsync<IJSObjectReference>("import", "./Pages/SpreadsheetGUI.razor.js"); // create/read the javascript
+            await JSModule.InvokeVoidAsync("SetDotNetInterfaceObject", DotNetObjectReference.Create(this)); // tell the javascript about us (dot net)
+            await JSModule.InvokeVoidAsync("TestJavaScriptInterop", "Hello JavaScript!"); // test that it is working.  You could remove this.
             await FormulaContentEditableInput.FocusAsync(); // when we start up, put the focus on the input. you will want to do this anytime a cell is clicked.
         }
 
-        Debug.WriteLine( $"{"OnAfterRender Done",-30}: {Navigator.Uri} - Remove Me." );
+        Debug.WriteLine($"{"OnAfterRender Done",-30}: {Navigator.Uri} - Remove Me.");
     }
 
     /// <summary>
@@ -163,7 +181,7 @@ public partial class SpreadsheetGUI
     /// <param name="cellName"> The name of the cell. </param>
     /// <param name="row"> The returned conversion between row and zero based index. </param>
     /// <param name="col"> The returned conversion between column letter and zero based matrix index. </param>
-    private static void ConvertCellNameToRowCol( string cellName, out int row, out int col )
+    private static void ConvertCellNameToRowCol(string cellName, out int row, out int col)
     {
         col = cellName[0] - 'A';
         row = int.Parse(cellName.Substring(1)) - 1;
@@ -176,7 +194,7 @@ public partial class SpreadsheetGUI
     /// <param name="row"> The row number (0-A, 1-B, ...).</param>
     /// <param name="col"> The column number (0 based).</param>
     /// <returns>A string defining the cell name, where the col is A-Z and row is not zero based.</returns>
-    private static string CellNameFromRowCol( int row, int col )
+    private static string CellNameFromRowCol(int row, int col)
     {
         return $"{Alphabet[col]}{row + 1}";
     }
@@ -187,7 +205,7 @@ public partial class SpreadsheetGUI
     /// <param name="newInput"> The new value to put at row/col. </param>
     /// <param name="row"> The matrix row identifier. </param>
     /// <param name="col"> The matrix column identifier. </param>
-    private async void HandleUpdateCellInSpreadsheet( string newInput, int row, int col )
+    private async void HandleUpdateCellInSpreadsheet(string newInput, int row, int col)
     {
         string cellName = CellNameFromRowCol(row, col);
         try
@@ -223,19 +241,19 @@ public partial class SpreadsheetGUI
     ///   </remarks>
     /// </summary>
     /// <param name="args"> Information about the file that has been selected. </param>
-    private async void HandleLoadFile( EventArgs args )
+    private async void HandleLoadFile(EventArgs args)
     {
         try
         {
             // FIXME: you only need to confirm if the sheet "dirty" (hasn't been changed)
             if (this.HasSpreadSheetChanged())
             {
-                return;
+                await JS.InvokeVoidAsync("alert", "You did not save your work");
             }
 
-            bool success = await JS.InvokeAsync<bool>( "confirm", "Do this?" );
+            bool success = await JS.InvokeAsync<bool>("confirm", "load your file?");
 
-            if ( !success )
+            if (!success)
             {
                 return;    // user canceled the action.
             }
@@ -243,10 +261,10 @@ public partial class SpreadsheetGUI
             string fileContent = string.Empty;
 
             InputFileChangeEventArgs eventArgs = args as InputFileChangeEventArgs ?? throw new Exception("that didn't work");
-            if ( eventArgs.FileCount == 1 )
+            if (eventArgs.FileCount == 1)
             {
                 var file = eventArgs.File;
-                if ( file is null )
+                if (file is null)
                 {
                     return;
                 }
@@ -255,7 +273,7 @@ public partial class SpreadsheetGUI
                 using var reader = new System.IO.StreamReader(stream);
                 fileContent = await reader.ReadToEndAsync();
 
-                await JS.InvokeVoidAsync( "alert", fileContent );
+                await JS.InvokeVoidAsync("alert", fileContent);
 
                 string tempFilePath = Path.GetTempFileName();
                 File.WriteAllText(tempFilePath, fileContent);
@@ -274,9 +292,9 @@ public partial class SpreadsheetGUI
                 StateHasChanged();
             }
         }
-        catch ( Exception e )
+        catch (Exception e)
         {
-            Debug.WriteLine( "something went wrong with loading the file..." + e );
+            Debug.WriteLine("something went wrong with loading the file..." + e);
         }
     }
 
@@ -299,12 +317,15 @@ public partial class SpreadsheetGUI
     {
         // <remarks> this null check is done because Visual Studio doesn't understand
         // the Blazor life cycle and cannot assure of non-null. </remarks>
-        if ( JSModule is not null )
+        if (JSModule is not null)
         {
-            var success = await JSModule.InvokeAsync<bool>( "saveToFile", "testfile.txt", "hello world" );
+            string tempFilePath = Path.Combine(Path.GetTempPath(), FileSaveName);
+            this.currentSpreadSheet!.Save(tempFilePath);
+            string fileContent = File.ReadAllText(tempFilePath);
+            var success = await JSModule.InvokeAsync<bool>("saveToFile", FileSaveName, fileContent);
             if (success)
             {
-                ShowHideSaveGUI( false );
+                ShowHideSaveGUI(false);
                 StateHasChanged();
             }
         }
@@ -327,24 +348,24 @@ public partial class SpreadsheetGUI
                 }
             }
 
-            // Clear the spreadsheet contents
-            this.currentSpreadSheet = new Spreadsheet(); // Reinitialize the spreadsheet
-
-            // Clear the GUI backing store to reflect the cleared spreadsheet
+            this.currentSpreadSheet = new Spreadsheet();
             for (int row = 0; row < CellsBackingStore.GetLength(0); row++)
             {
                 for (int col = 0; col < CellsBackingStore.GetLength(1); col++)
                 {
-                    CellsBackingStore[row, col] = string.Empty; // Clear cell contents
-                    CellsClassBackingStore[row, col] = string.Empty; // Clear cell class if needed
+                    CellsBackingStore[row, col] = string.Empty;
+                    CellsClassBackingStore[row, col] = string.Empty;
                 }
             }
+
             InputWidgetBackingStore = string.Empty;
             StateHasChanged();
         }
     }
 
-    // Initialize backing stores
+    /// <summary>
+    /// this is a helper method to Initialize Backing Stores.
+    /// </summary>
     private void InitializeBackingStores()
     {
         for (int row = 0; row < inputRows; row++)
@@ -357,6 +378,11 @@ public partial class SpreadsheetGUI
         }
     }
 
+    /// <summary>
+    /// This method can set cell's value using cell's name.
+    /// </summary>
+    /// <param name="cellName">the cell need to be set value.</param>
+    /// <returns>returns cell's data.</returns>
     private object SetValueForCellsBackingStore(string cellName)
     {
         object curVal = this.currentSpreadSheet?.GetCellValue(cellName) ?? "null variable";
@@ -373,6 +399,12 @@ public partial class SpreadsheetGUI
         return this.CellsBackingStore[row, col];
     }
 
+    /// <summary>
+    /// This method can set cell's value using cell's row and col.
+    /// </summary>
+    /// <param name="row">the row of the cell.</param>
+    /// <param name="col">colunm of the cell.</param>
+    /// <returns>the value of the cell.</returns>
     private string SetValueForCellsBackingStore(int row, int col)
     {
         string cellName = CellNameFromRowCol(row, col);
@@ -380,6 +412,12 @@ public partial class SpreadsheetGUI
         return value;
     }
 
+    /// <summary>
+    /// get cell's content.
+    /// </summary>
+    /// <param name="row">the row of cell.</param>
+    /// <param name="col">the colunm of cell.</param>
+    /// <returns>returns content of cell. If cell's content is formula, the string will start with a "=".</returns>
     private string TurnContenCellSameAsSetContent(int row, int col)
     {
         string cellName = CellNameFromRowCol(row, col);
